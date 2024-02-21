e949a69a533922076c7549652d35dd0679d56b7a
mustafasu498 <mustafasu498@gmail.com>
2024-02-21 23:03:29 +0500

    Assignment 16 Done

---
+++ app.js
@@ -0,0 +1,89 @@
+// Chapter No 1
+// Q No 1
+
+alert("Error! Please Enter a valid password")
+
+
+// Q No 2
+
+alert("Welcome to JS Land \n Happy Coding!")
+
+//  Q No 3
+
+alert("Welcome to JS Land...")
+
+//  Q No 4
+
+alert("Happy Coding! \n  Prevent this page from creating additional dailogs.")
+
+// Q No 5
+
+alert("Hello... I can run JS thourgh web browser's console")
+
+
+
+// Chapter No 2
+
+// Q No 1
+
+var MyName = "Ghulam Mustafa" , age = 18 , Course = "Certifed Mobile Application Development"
+
+alert(MyName)
+alert(age)
+alert(Course)
+
+// Q No 3
+
+var name1 = "Pizza", name2 = "Pizz", name3 = "Piz", name4 = "Pi" , name5 = "P"
+
+alert(name1 +"\n"+name2 + "\n" + name3 + "\n" + name4 + "\n" + name5   )
+
+// Q No 4
+
+var email = "mustafasu498@gmail.com"
+
+alert("my email address is"+ email)
+
+// Q No 5
+
+var book = " A smarter way to learn JavaScript"
+
+alert("iam tring to learn from the Book" + book)
+
+// Q No 6
+
+var message = "Yah! I can Write HTML Content through Javascript"
+
+document.write(message)
+
+// Q No 7
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert(design)
+
+// Chapter No 3
+
+// Q No 1
+
+var age = 18
+
+alert("iam" + age + "years old")
+
+// Q No 2
+
+var visit = 14
+
+alert("you have visited this website "+ visit + "times")
+// Q3
+var birthdy =2006
+document.write(" My birth year is " + birthdy)
+document.write("<br>")
+// Q4
+var VisitorName = "<b>John Doe </b>"
+var Product= " <b>T-shirt (s)</b>"
+var Quantity = 5
+document.write(VisitorName +"ordered " + Quantity + Product + "XYZ Clothing store")
+var FirstName= "ghulam", LastName= "mustafa",Email="mustafasu498@gmail.com",PhoneNumber =12345678910, password=123456789
+alert( "firstname "+ FirstName + "\n" + "lastname " + LastName + "\n" + "email " + Email +"\n"+ "pohne number "+ PhoneNumber +"\n" + "password "  + password )
+ 
